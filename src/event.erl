67c32e706c28dddd9c454ff80ddcf1222053ca29
Chrimm <ripitrust@gmail.com>
2017-02-03 01:28:04 +0800

    finish event

--- src/event.erl
+++ src/event.erl
@@ -1,14 +1,65 @@
 -module(event).
 -compile(export_all).
 -record(state, {server, 
-				name="",
-				to_go=0}).
+                name="",
+                to_go=0}).
+
+normalize(N) -> 
+    Limit = 49* 24 * 60 * 60,
+    [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
+
+    receive
+        {Server, Ref, cancel} ->
+            Server ! {Ref, ok}
+    after T * 1000 ->
+        if Next =:= [] ->
+                Server ! {done, S#state.name};
+           Next =/= [] ->
+                loop(S#state{to_go=Next})
+    end
+
+end.
 
 
-loop(S = #state{server=Server}) ->
-	receive
-		{Server, Ref, cancel} ->
-			Server ! {Ref, ok}
-	after S#state.to_go*1000 ->
-			Server ! {done, S#state.name}
-	end.
+start(EventName, Delay) ->
+    spawn(?MODULE, init, [self(), EventName, Delay]).
+
+
+start_link(EventName, Delay) ->
+    spawn_link(?MODULE, init, [self(), EventName, Delay]).
+
+init(Server, EventName, Delay) ->
+    loop(#state{server=Server,
+                name=EventName,
+                to_go=normalize(Delay)}).
+
+
+cancel(Pid) ->
+    
+    Ref = erlang:monitor(process, Pid),
+    Pid ! {self(), Ref, cancel},
+    receive
+        {Ref, ok} ->
+
+            erlang:demonitor(Ref, [flush]),
+            ok;
+        {'DOWN', Ref, process, Pid, _Reason} ->
+            ok
+
+    end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
